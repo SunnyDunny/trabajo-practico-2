bc24fcb9c5f19d166be8fccbcb4b313930397b4d
Sol M. Sánchez <somarielsanchez14@gmail.com>
2024-10-27 20:38:37 -0300

    popover inicializado

--- js/galeria.js
+++ js/galeria.js
@@ -28,4 +28,8 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+//Inicializar popovers
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 
